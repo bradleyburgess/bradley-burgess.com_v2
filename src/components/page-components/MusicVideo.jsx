631bff8d2d89ab7cac7e2b82d93685fb3bb7a2ce
Bradley Burgess <47909061+bradleyburgess@users.noreply.github.com>
2021-09-02 10:54:21 -0400

    Fixed new links (didn't have embed)

--- src/components/page-components/MusicVideo.jsx
+++ src/components/page-components/MusicVideo.jsx
@@ -39,42 +39,42 @@ const videoList = [
   {
     title: 'Jacob Weber: Two Settings on "Antioch"',
     duration: "3:49",
-    url: "https://www.youtube.com/watch?v=Ge3K1gByNGQ",
+    url: "https://www.youtube.com/embed/Ge3K1gByNGQ",
   },
   {
     title: 'Richard Purvis: Prelude on "Greensleeves"',
     duration: "4:09",
-    url: "https://www.youtube.com/watch?v=v1BUwVxm4FA",
+    url: "https://www.youtube.com/embed/v1BUwVxm4FA",
   },
   {
     title: 'Richard Elliott: Toccatina on "Angels We Have Heard"',
     duration: "1:25",
-    url: "https://www.youtube.com/watch?v=FayQsx2IjQU",
+    url: "https://www.youtube.com/embed/FayQsx2IjQU",
   },
   {
     title: 'Andrew Carter: Toccata on "Veni Emmanuel"',
     duration: "3:42",
-    url: "https://www.youtube.com/watch?v=bD7wm5GWxrQ",
+    url: "https://www.youtube.com/embed/bD7wm5GWxrQ",
   },
   {
     title: 'Brahms: Es ist ein Ros entsprungen',
     duration: "2:46",
-    url: "https://www.youtube.com/watch?v=8IhuAbw_YIw",
+    url: "https://www.youtube.com/embed/8IhuAbw_YIw",
   },
   {
     title: 'J.S. Bach: In dulci jubilo, BWV 729',
     duration: "2:35",
-    url: "https://www.youtube.com/watch?v=G0PbQc3h8y4",
+    url: "https://www.youtube.com/embed/G0PbQc3h8y4",
   },
   {
     title: 'Richard Purvis: Carol Rhapsody',
     duration: "4:01",
-    url: "https://www.youtube.com/watch?v=4vYCOTOBfJg",
+    url: "https://www.youtube.com/embed/4vYCOTOBfJg",
   },
   {
     title: 'Improvisation: "Let everything that has breath"',
     duration: "2:25",
-    url: "https://www.youtube.com/watch?v=QTCYr53x64Q&list=PLxwU2G1FQnySW4-SWaU4p7Qj0qhFUz8SD&index=3"
+    url: "https://www.youtube.com/embed/QTCYr53x64Q&list=PLxwU2G1FQnySW4-SWaU4p7Qj0qhFUz8SD&index=3"
   }
 ]
 
